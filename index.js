3f69f36aa0544ddb36b63232faf2f399ccf70569
Akhil Bharti <akhil.bh96@gmail.com>
2019-09-12 01:07:02 +0530

    reorg code and added start script

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const router = require('./routes/faucetRoutes')
-const config = require('./config')
-
-app.use('/', router)
-
-app.listen(config.PORT, () => console.log(`Rinkeby faucet started on ${config.PORT}:`))
